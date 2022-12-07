1bb40a7bd9f3ca8c06098a345153ed19ad2ad99a
aowuor <angelaowuor@gmail.com>
2022-12-07 13:40:15 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  document.querySelector('form').addEventListener("submit", function(e){
+    e.preventDefault();
+    buildToDo(e.target.new_task_description.value);
+    e.target.new_task_description.value = " "
+  })
+})
+
+function buildToDo(todo){
+  let toDoList = document.createElement("li")
+  let btn = document.createElement("button")
+  btn.addEventListener("click", handleDelete)
+  toDoList.textContent = `${todo}`
+  btn.textContent = "Delete"
+  toDoList.appendChild(btn)
+  document.querySelector("#tasks").appendChild(toDoList)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove();
+}
+
+
